67bf844c19350f1f557a9806c8d12879ea653e53
Joao Carlos <buenotjoao07@gmail.com>
2024-08-31 11:18:29 -0300

    Primeiro commit da aplicação - Joao Carlos

--- PlantechWeb/Program.cs
+++ PlantechWeb/Program.cs
@@ -15,7 +15,6 @@ namespace PlantechWeb
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -26,9 +25,7 @@ namespace PlantechWeb
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
