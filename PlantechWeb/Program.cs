4f059840e12395ab9755ca736577dec4f8df86e7
Joao Carlos <buenotjoao07@gmail.com>
2024-08-31 14:40:34 -0300

    testando branches

--- PlantechWeb/Program.cs
+++ PlantechWeb/Program.cs
@@ -4,7 +4,7 @@ namespace PlantechWeb
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args); //teste
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
